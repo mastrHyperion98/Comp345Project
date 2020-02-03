24762f7cae6293b2f5651324fb2c6a2548023044
Amadeus <s_smith1998@outlook.com>
2020-02-02 22:03:21 -0500

    Add GB_Tile header

---
+++ NewHaven/source/GB_Tile.h
@@ -0,0 +1,17 @@
+//
+// Created by Steven on 2020-02-02
+// Created for Concordia W 2020 Comp 345 New Haven project
+// Header file containing the interface for the GameBoard Tiles (Harvest or Pond)
+//
+
+#ifndef NEWHAVEN_GB_TILE_H
+#define NEWHAVEN_GB_TILE_H
+
+#endif //NEWHAVEN_GB_TILE_H
+
+class GB_Tile{
+    public:
+        // default Constructor
+        GB_Tile();
+    private:
+};
