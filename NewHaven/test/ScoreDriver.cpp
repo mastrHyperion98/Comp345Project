f03ec75b4f14fe49e626bb3e3da89f1d216805e4
mastrHyperion98 <s_smith1998@outlook.com>
2020-03-14 04:24:40 -0400

    Add a non-loop test too

--- NewHaven/test/ScoreDriver.cpp
+++ NewHaven/test/ScoreDriver.cpp
@@ -16,7 +16,7 @@ using namespace boost;
 int main()
 {
     GBMap map(0);
-
+    GBMap map2(0);
 /*
  * -  00 01 02 03 04 -
  * -  05 06 07 08 09 -
@@ -54,6 +54,11 @@ int main()
     map.placeHarvestTile(16, *(new HarvestTile(tile6)));
     map.placeHarvestTile(17, *(new HarvestTile(tile7)));
     map.placeHarvestTile(18, *(new HarvestTile(tile8)));
+    map2.placeHarvestTile(6, *(new HarvestTile(tile4)));
+    map2.placeHarvestTile(7, *(new HarvestTile(tile3)));
+    map2.placeHarvestTile(8, *(new HarvestTile(tile2)));
+    map2.placeHarvestTile(11, *(new HarvestTile(tile5)));
+    map2.placeHarvestTile(13, *(new HarvestTile(tile1)));
 
     /*Creating a variable for every tile to store its content for the sole purpose of printing it to console.
     * They are all pointers because tileContent is an array.
@@ -94,10 +99,10 @@ int main()
 
     ResourceCalculator calculator;
     map.printIndexConfiguration();
-    cout << "\n***The Asterisk denotes the position 13!***\n" << endl;
+    cout << "\n***MAP 1: The Asterisk denotes the position 13!***\n" << endl;
     int root;
     bool valid_root = false;
-    for(int i = 0; i < 7; i++) {
+    for(int i = 0; i < 3; i++) {
         while (cout << " Please input a proper root from which to compute the resources gathered: " && !(cin >> root)) {
             std::cin.clear(); //clear bad input flag
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
@@ -113,6 +118,35 @@ int main()
         }
         if(valid_root) {
            int* re = calculator.computeResources(
+                    *map.getResourcedGraph(root)); //13 is the root tile, can be changed to any other valid tile.
+            cout << '\n';
+            cout << re[0] << "\t" << re[1]<< "\t" << re[2] << "\t" << re[3] << endl;
+        }
+    }
+
+
+    std::cout << '\n' << "ST\tSH\tSH\tSH\tSH\tSH" << '\n'
+              << "ST\tWD\tWD\tST\tWD\tSH" << '\n'
+              << "ST\tWD\t \t \t*SH\tSH" << '\n'
+              << "SH\tWD\t \t \tSH\tWD" << '\n' << endl;
+    cout << "\n***MAP 2: The Asterisk denotes the position 13!***\n" << endl;
+    valid_root = false;
+    for(int i = 0; i < 3; i++) {
+        while (cout << " Please input a proper root from which to compute the resources gathered: " && !(cin >> root)) {
+            std::cin.clear(); //clear bad input flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard input
+            std::cout << "Invalid input; please re-enter.\n";
+        }
+
+        if (root >= 5 && root != 12 && root != 15 && root <= 19)
+            valid_root = true;
+        else {
+            std::cout << "Invalid input; please re-enter.\n";
+            valid_root = false;
+            i--;
+        }
+        if(valid_root) {
+            int* re = calculator.computeResources(
                     *map.getResourcedGraph(root)); //13 is the root tile, can be changed to any other valid tile.
             cout << '\n';
             cout << re[0] << "\t" << re[1]<< "\t" << re[2] << "\t" << re[3] << endl;
